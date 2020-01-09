cc5c69b3c9bd5a40f972e91213086ef35c748bc6
Pranav <pranavpaharia@gmail.com>
2020-01-09 20:50:49 +0530

    Added Module dependency

--- Source/CarCoaloBP/CarCoaloBP.Build.cs
+++ Source/CarCoaloBP/CarCoaloBP.Build.cs
@@ -10,7 +10,7 @@ public class CarCoaloBP : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "ThoughtfishCoalaPlugin" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
