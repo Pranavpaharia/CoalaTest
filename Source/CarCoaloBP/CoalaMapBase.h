9241930bdb4dce4476fbebb1181c8cfc4a571194
Pranav <pranavpaharia@gmail.com>
2020-01-09 20:50:12 +0530

    AddedCoala Map base class

--- Source/CarCoaloBP/CoalaMapBase.h
+++ Source/CarCoaloBP/CoalaMapBase.h
@@ -3,8 +3,21 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "CoalaRemoteTileRequest.h"
 #include "GameFramework/Actor.h"
 #include "CoalaMapBase.generated.h"
+
+
+UENUM(BlueprintType)
+enum class EMapExpansion : uint8
+{
+	NorthExp,
+	SouthExp,
+	EastExp,
+	WestExp,
+	None
+};
+
 
 UCLASS()
 class CARCOALOBP_API ACoalaMapBase : public AActor
@@ -14,6 +27,7 @@ class CARCOALOBP_API ACoalaMapBase : public AActor
 public:	
 	// Sets default values for this actor's properties
 	ACoalaMapBase();
+	EMapExpansion mapExpansionMode;
 
 protected:
 	// Called when the game starts or when spawned
@@ -22,5 +36,42 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadWrite, Category = "MapTiles")
+	TArray<FCoalaRemoteTileRequest> TileList;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "MapTiles")
+	TArray<FCoalaRemoteTileRequest> EastCoastTileList;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "MapTiles")
+	TArray<FCoalaRemoteTileRequest> WestCoastTileList;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "MapTiles")
+	TArray<FCoalaRemoteTileRequest> NorthCoastTileList;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "MapTiles")
+	TArray<FCoalaRemoteTileRequest> SouthCoastTileList;
+
+
+	UFUNCTION(BlueprintCallable, Category = "MapTiles")
+	void InitializeMapList(bool bResetMainList);
+
+	UFUNCTION(BlueprintCallable, Category = "MapTiles")
+	void CreateMapTileDataBase(TArray<FCoalaRemoteTileRequest> newAreasInRange);
+
+	UFUNCTION(BlueprintCallable, Category = "MapTiles")
+	void NeedToExpandMapInDir(EMapExpansion ExpansionMode, TArray<FCoalaRemoteTileRequest>& newTileArray);
+
+	void ShowListItems(TArray<FCoalaRemoteTileRequest> mList);
+
+	void IntializeCoalaServices();
+
+	void SpawnCenteralActor();
+
+public:
+    //For the variables only
+	float CoalaMainScale = 1.f;
+	FString CoalaAPIKey = TEXT("5F799927B35D0544");
+	AActor* MapCentralActor;
 
 };
